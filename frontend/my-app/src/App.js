911219b54ac361ca5043a7175e6268dc82bbe86e
Varun Kumar <varunkumar17135176@gmail.com>
2026-05-17 14:04:44 +0530

    conditional routing in app.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,43 +7,55 @@ import About from "./views/homepage/about.jsx";
 import Features from "./views/homepage/feature";
 import Contacts from "./views/homepage/contacts";
 import HowItWorks from "./views/homepage/howItWorks";
-import Navbar from "./views/homepage/navbar";
-import Footer from "./views/homepage/footer";
 import Mainlayout from "./views/mainlayout";
 import Create from "./views/createsession/create.jsx";
 import Join from "./views/joinsession/join.jsx";
 import Position from "./views/myposition/position.jsx";
 import Next from "./views/next/next.jsx";
 import Auth from "./views/Auth.jsx";
-import Dashboard from "./views/dashboard/Dashboard.jsx";
 import Content from "./views/Content.jsx";
 import Profile from "./views/profile/Profile.jsx";
 import Default from "./views/Default.jsx";
+import { Navigate } from "react-router-dom";
+
 function App() {
+  const token=localStorage.getItem("token");
+
   return (
     <div>
       <Routes>
-        <Route element={<Mainlayout/>}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/how-it-works" element={<HowItWorks />} />
-        <Route path="/contact" element={<Contacts />} />
+
+        <Route element={<Mainlayout />}>
+          <Route path="/" element={token ? <Navigate to="/profile" /> : <Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/contact" element={<Contacts />} />
         </Route>
-        <Route element={<Auth></Auth>}>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/signup" element={<Signin/>}></Route>
+
+  
+        <Route element={<Auth />}>
+          <Route path="/login" element={!token ? <Login /> : <Navigate to="/profile" />} />
+          <Route path="/signup" element={!token ? <Signin /> : <Navigate to="/profile" />} />
         </Route>
-        <Route element={<Content/>}>
-        <Route path='/profile' element={<Profile></Profile>}></Route>
-         <Route path="/createsession" element={<Create/>}></Route>
-         <Route path="/joinsession" element={<Join/>}></Route>
-         <Route path="/position" element={<Position/>}></Route>
-         <Route path="/next" element={<Next/>}></Route>
+
+        
+        <Route
+          element={token ? <Content /> : <Navigate to="/" />}
+        >
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/createsession" element={<Create />} />
+          <Route path="/joinsession" element={<Join />} />
+          <Route path="/position" element={<Position />} />
+          <Route path="/next" element={<Next />} />
         </Route>
-        <Route path="*" element={<Default></Default>}></Route>
+
+  
+        <Route path="*" element={<Default />} /> 
+
       </Routes>
     </div>
   );
 }
+
 export default App;
