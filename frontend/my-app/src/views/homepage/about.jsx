e30d5c8f65ba54f5da0ca71f6ac7edf24d9c5f57
Varun Kumar <varunkumar17135176@gmail.com>
2026-05-17 14:48:50 +0530

    about,contact,feature page

---
+++ frontend/my-app/src/views/homepage/about.jsx
@@ -0,0 +1,12 @@
+import style from './about.module.css';
+function About(){
+  return(
+    <div>
+      <div className={style.main}>
+        <h2>About</h2>
+        <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam nobis pariatur ea corporis dolorem sequi nostrum labore officia aut quis alias cumque eum ex eius, soluta sed incidunt libero blanditiis tempore fugit optio dolores assumenda iusto quam? Nemo sequi inventore temporibus impedit totam, aspernatur adipisci ut minus non quaerat! Officia, tempore? Quis sequi expedita temporibus vitae ea, quidem animi. Quas enim perspiciatis, cupiditate, provident soluta sint pariatur impedit maxime repellendus reprehenderit dicta rem voluptatibus ex odit repellat aliquam nam voluptatum, voluptate quod est corrupti quia. Amet sequi voluptatibus voluptatum expedita, officia itaque quae aut dolorem tenetur ex accusamus blanditiis atque?</h5>
+      </div>
+    </div>
+  );
+}
+export default About;
