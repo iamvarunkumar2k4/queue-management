3af7fa0e984bc6058738ad41b123ba2ab1fd3b6d
Varun Kumar <varunkumar17135176@gmail.com>
2026-05-19 16:33:35 +0530

    correct basenamenin react router

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from './Appcontext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <AppProvider>
       <App></App>
     </AppProvider>
